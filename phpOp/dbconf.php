b1d16911ab45b6c3cfcc9f66b84bebad3873fda1
Edmund Jay <ejay@mgi1.com>
2019-10-03 14:05:40 -0700

    Updated DB conf files

--- phpOp/dbconf.php
+++ phpOp/dbconf.php
@@ -26,9 +26,7 @@ define('DB_PASSWORD',           '');
 define('DB_HOST',               'localhost');
 define('DB_PORT',               '3306');
 define('DB_DATABASE',           'phpoidc_01');
-define('DB_CONNECTION_NAME',    'phpoidc_connection');
 
 
 define("DSN", DB_TYPE . '://' . DB_USER . ':' . DB_PASSWORD . '@' . DB_HOST . ':' . DB_PORT . '/' . DB_DATABASE );
-define('DB_CONNECTION',    Doctrine_Manager::connection(DSN, DB_CONNECTION_NAME));
 ?>
